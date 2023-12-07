64e8569a2b4e205df1047bbe02c0dfabb59064a6
sammcmurtrey <mcmurtreysamantha@gmail.com>
2023-12-06 20:19:36 -0600

    //

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/RegisterFormDTO.java
@@ -0,0 +1,25 @@
+package org.launchcode.javawebdevtechjobsauthentication.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+}
